c5a78d1d2628005fef3f455a234c14af51573773
Kenan Yildirim <kenan@kenany.me>
2018-10-10 21:39:18 -0400

    set up eslint
    Closes https://github.com/MemeLabs/chat-gui/issues/27
    Closes https://github.com/MemeLabs/chat-gui/pull/29

---
+++ assets/.eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    globals: {
+        API_URI: false,
+        LOGIN_URI: false,
+        WEBSOCKET_URI: false
+    },
+    env: {
+        browser: true,
+        jquery: true
+    }
+};
