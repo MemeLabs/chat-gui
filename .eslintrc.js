ed45152ad9e0e967e5e0277920ba50ede5aafe98
Kenan Yildirim <kenan@kenany.me>
2018-10-10 21:39:18 -0400

    set up eslint
    Closes https://github.com/MemeLabs/chat-gui/issues/27

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+    extends: 'semistandard',
+    rules: {
+        'block-spacing': 1,
+        'brace-style': 1,
+        'camelcase': 1,
+        'comma-dangle': 1,
+        'comma-spacing': 1,
+        'curly': 1,
+        'dot-location': 1,
+        'eol-last': 1,
+        'eqeqeq': 1,
+        'indent': [1, 4],
+        'key-spacing': 1,
+        'keyword-spacing': 1,
+        'no-cond-assign': 1,
+        'no-extra-boolean-cast': 1,
+        'no-extra-semi': 1,
+        'no-inner-declarations': 1,
+        'no-multi-spaces': 1,
+        'no-multiple-empty-lines': 1,
+        'no-path-concat': 1,
+        'no-return-assign': 1,
+        'no-trailing-spaces': 1,
+        'no-unused-vars': 1,
+        'no-useless-escape': 1,
+        'object-property-newline': 1,
+        'one-var': 1,
+        'operator-linebreak': 1,
+        'padded-blocks': 1,
+        'quotes': 1,
+        'semi': 1,
+        'space-before-blocks': 1,
+        'space-before-function-paren': 0,
+        'space-in-parens': 1,
+        'space-infix-ops': 1,
+        'space-unary-ops': 1,
+        'spaced-comment': 1,
+
+        'promise/param-names': 1
+    }
+};
