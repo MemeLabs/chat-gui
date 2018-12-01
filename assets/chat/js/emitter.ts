b9ed2190c6d0459fde3bfb7b03d0d9f8281c520d
Kenan Yildirim <kenan@kenany.me>
2018-12-01 14:14:24 -0500

    convert EventEmitter to typescript

--- assets/chat/js/emitter.ts
+++ assets/chat/js/emitter.ts
@@ -1,15 +1,17 @@
 class EventEmitter {
+    private readonly listeners: Map<string, Array<(...args: unknown[]) => void>>;
+
     constructor() {
         this.listeners = new Map();
     }
 
-    on(name, fn) {
+    on(name: string, fn: (...args: unknown[]) => void) {
         this.listeners.has(name) || this.listeners.set(name, []);
-        this.listeners.get(name).push(fn);
+        this.listeners.get(name)!.push(fn);
         return this;
     }
 
-    emit(name, ...args) {
+    emit(name: string, ...args: unknown[]) {
         const listeners = this.listeners.get(name);
         if (listeners && listeners.length) {
             listeners.forEach(listener => listener(...args));
