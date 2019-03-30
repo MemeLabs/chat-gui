1ab7a43f3629da51e8a4dc11ce53b3f665f947c7
xDashh <Dashh@protonmail.com>
2019-03-30 17:11:27 +0100

    add generify-flip and clean up css (#84)

--- assets/chat/js/const.js
+++ assets/chat/js/const.js
@@ -51,6 +51,7 @@ function isKeyCode(e, code) {
 // emote:key; css class to apply
 const GENERIFY_OPTIONS = {
     'mirror': 'mirror',
+    'flip': 'flip',
     'rain': 'weather rain anim-fix',
     'snow': 'weather snow anim-fix',
     'rustle': 'generify-rustle',
