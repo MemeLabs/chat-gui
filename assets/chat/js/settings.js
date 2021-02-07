eec18929a27575885b114ada8cca46ccb762bb52
Ftwpala <ftwpala@abv.bg>
2021-02-07 13:48:34 +0100

    Added modifier to all emotes, added setting to toggle.

--- assets/chat/js/settings.js
+++ assets/chat/js/settings.js
@@ -38,6 +38,7 @@ function upgradeSettings(chat, oldversion, newversion) {
         chat.settings.set("soundnotificationhighlight", arr);
         chat.settings.set("soundnotificationwhisper", arr);
         chat.settings.set("notificationsoundfile", arr);
+        chat.settings.set("holidayemotemodifiers", arr);
 
         arr = chat.settings.get("notificationtimeout");
         chat.settings.set("notificationtimeout", arr !== -1);
